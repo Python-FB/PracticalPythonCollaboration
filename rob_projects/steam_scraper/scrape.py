858757545b93db395c9cf68b3b87cf9da6446179
Rob Weddell <rweddell4@gmail.com>
2022-04-05 21:10:05 -0500

    Comments added to script

--- rob_projects/steam_scraper/scrape.py
+++ rob_projects/steam_scraper/scrape.py
@@ -1,18 +1,14 @@
 import requests
-# from lxml import html as HTML
-# from lxml.etree import HTMLParser
 import lxml.html
-# import lxml
 import json
 
 page_data = requests.get('https://store.steampowered.com/explore/new/')
-# page = HTML.fromstring(page_data.content, parser=HTMLParser(encoding='utf8'))
 page = lxml.html.fromstring(page_data.content)
-# page = lxml.etree._Element(page)
 new_games = page.xpath('//div[@id="tab_newreleases_content"]')[0]
 
 titles = new_games.xpath('.//div[@class="tab_item_name"]/text()')
 
+#NOTE I wrote this function to clean out some non-ascii characters
 def remove_unicode(title):
     title = title.encode('ascii', 'ignore')
     title = title.decode()
@@ -24,14 +20,16 @@ prices = new_games.xpath('.//div[@class="discount_final_price"]/text()')
 
 tags = []
 for tag in new_games.xpath('.//div[@class="tab_item_top_tags"]'):
-    tags.append(tag.text_content())
-tags = [tag.split(', ') for tag in tags]
+    tags.append(tag.text_content().split(', '))
 
 platform_divs = new_games.xpath('.//div[@class="tab_item_details"]')
 total_platforms = []
 counter = 0
 for game in platform_divs:
     temp = game.xpath('.//span[contains(@class, "platform_img")]')
+
+    # NOTE: I changed up this code to make it more efficient
+    # Iterates through the list only once
     platforms = []
     for platform in temp:
         holder = platform.get('class').split(' ')[-1]
@@ -45,6 +43,9 @@ output = {}
 headers = ['title', 'price', 'tags', 'platforms']
 counter = 0
 for info in zip(titles, prices, tags, total_platforms):
+
+    # NOTE: I changed the output a bit to have a pure dictionary
+    # Instead of a list of json objects, I made a dictionary of json objects
     resp = {}
     for i in range(1,4):
         resp[headers[i]] = info[i]
