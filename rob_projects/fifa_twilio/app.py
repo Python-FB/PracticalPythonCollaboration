51dd9a5c3cbd17488f6c080f2a2b7b5195e3a64e
Rob Weddell <rweddell4@gmail.com>
2022-04-15 14:20:12 -0500

    Flask app initital

---
+++ rob_projects/fifa_twilio/app.py
@@ -0,0 +1,36 @@
+import requests
+from flask import Flask, request
+import os
+from dateutil import parser, tz
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+
+urls = {'group': 'https://worldcup.sfg.io/teams/group_results',
+        'country': 'https://worldcup.sfg.io/matches/country?fifa_code=',
+        'today': 'https://worldcup.sfg.io/matches/today',
+        'tomorrow': 'https://worldcup.sfg.io/matches/tomorrow'
+}
+
+countries = ['KOR', 'PAN', 'MEX', 'ENG', 'COL', 'JPN', 'POL', 'SEN',
+            'RUS', 'EGY', 'POR', 'MAR', 'URU', 'KSA', 'IRN', 'ESP',
+            'DEN', 'AUS', 'FRA', 'PER', 'ARG', 'CRO', 'BRA', 'CRC',
+            'NGA', 'ISL', 'SRB', 'SUI', 'BEL', 'TUN', 'GER', 'SWE']
+
+# html = requests.get(urls['today']).json()
+
+# app = Flask('FILIO')
+
+app = Flask(__name__)
+to_zone = tz.gettz('America/New_York')
+
+@app.route('/', methods=['POST'])
+def receive_sms():
+    body = request.values.get('Body', None)
+    resp = MessagingResponse()
+    resp.message(body or 'Hello World!')
+    return str(resp)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
